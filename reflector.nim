df19d0b71a507c2a5824ded8027be2b4370ab3ce
Jason Beetham <beefers331@gmail.com>
2023-11-20 23:59:06 -0700

    Router (#1)
    * Cleaned up code to use a reusable route table, might be illadvised
    
    * Directories now sync and do not crash
    
    * Moved logic to be more reusable

--- reflector.nim
+++ reflector.nim
@@ -19,12 +19,21 @@ import std/inotify except InotifyEvent
 import os except getConfigDir, getCacheDir
 import std/logging
 import pkg/slicerator
+import reflector/inotifyevents
 # import pkg/syslog # Does using syslog make any sense‽
+
+type Reflector* = ref object
+  handlers*: Table[InotifyFlags, proc(_: Reflector, _: ptr InotifyEvent, src: Path): Future[void] {.async.}]
+  mirrors*: Table[Path, HashSet[Path]]
+  watchers*: Table[cint, Path]
+  configWatchers*: Table[cint, Path]
+  movedFromBuffer*: Table[uint32, (Path, Path)] # (Parent, Name)
+  futures*: seq[Future[void]]
+  watcher*: cint
+
 
 type Unimplemented = object of CatchableError
 
-addHandler newConsoleLogger()
-addHandler newFileLogger(string (getCacheDir() / Path"reflector"))
 
 proc error(s: string) =
   try:
@@ -47,10 +56,8 @@ proc fatal(s: string) =
   except Exception as e:
     echo "Failed to log FATAL: ", e.msg
 
-
 template unimplemented(s: string): untyped =
   raise (ref Unimplemented)(msg: "Unimplemented: " & s)
-
 
 const 
   pathScan = "$+->$+"
@@ -62,62 +69,11 @@ let cfgPaths =
   ]
 
 
-type
-  InotifyFlag = enum
-    Access = 1
-    Modify
-    Attrib
-    CloseWrite
-    CloseNoWrite
-    Open
-    MovedFrom
-    MovedTo
-    Create
-    Delete
-    DeleteSelf
-    MoveSelf
-    
-    Unmount = 14
-    QueueOverflow
-    Ignored
-    
-    OnlyDir = 25
-    DontFollow
-    ExclUnlink
-    
-    OnlyCreateWatch = 29 
-    AddToWatch
-  
-    IsDir
-    OneShot
-
-  InotifyFlags = set[InotifyFlag]
-
-  InotifyEvent {.pure, final, importc: "struct inotify_event",
-                 header: "<sys/inotify.h>".} = object
-    wd* {.importc: "wd".}: FileHandle ## Watch descriptor.
-    mask* {.importc: "mask".}: InotifyFlags ## Watch mask.
-    cookie* {.importc: "cookie".}: uint32 ## Cookie to synchronize two events.
-    len* {.importc: "len".}: uint32 ## Length (including NULs) of name.
-    name* {.importc: "name".}: UncheckedArray[char] ## Name.
-
-converter toCint*(i: InotifyFlags): uint32 = copyMem(result.addr, i.addr, sizeof cint)
-
-when sizeof(InotifyFlags) != sizeof(uint32):
-  {.error: fmt"Mismatch between flags({sizeof InotifyFlags}) and uint32({sizeof(uint32)})".}
-
-proc getName*(evt: ptr InotifyEvent): string = $cast[cstring](evt.name.addr)
-
-const 
-  Close = {CloseWrite, CloseNoWrite}
-  Moved = {MovedFrom, MovedTo}
-
 proc hash(p: Path): Hash = hash(string p)
 proc `$`(p: Path): string = string p
 
-
-proc createWatcherRecursively(fd: cint, path: Path, watchers: var Table[cint, Path], mirrors: var Table[Path, HashSet[Path]], parent: Path) =
-  let watchdog = fd.inotify_add_watch(cstring path, {Create, Attrib, Modify} + Moved)
+proc createWatcherRecursively*(reflectObj: Reflector, path: Path, parent: Path) =
+  let watchdog = reflectObj.watcher.inotify_add_watch(cstring path, {Create, Attrib, Modify} + Moved)
   if watchdog < 0:
     try:
       error(fmt"Failed to watch '{string path}': {osErrorMsg(osLastError())}.")
@@ -126,20 +82,20 @@ proc createWatcherRecursively(fd: cint, path: Path, watchers: var Table[cint, Pa
 
   if parent != Path"":
     let 
-      parentPaths = mirrors[parent]
+      parentPaths = reflectObj.mirrors[parent]
       tail = path.splitPath.tail
     var destPaths = initHashSet[Path](parentPaths.len)
     for parDir in parentPaths:
       destPaths.incl parDir / tail
-    mirrors[path] = destPaths
+    reflectObj.mirrors[path] = destPaths
 
-  watchers[watchdog] = path
+  reflectObj.watchers[watchdog] = path
   if dirExists path:
     for dir in path.walkDir():
       if dir.kind == pcDir:
-        createWatcherRecursively(fd, dir.path, watchers, mirrors, path)
+        createWatcherRecursively(reflectObj, dir.path, path)
 
-proc clone(src, dest: Path, srcModifcation: Time, creating: bool = false): Future[void] {.async.} =
+proc clone*(src, dest: Path, srcModifcation: Time, creating: bool = false): Future[void] {.async.} =
   if creating:
     info fmt"Created '{dest}' due to '{src}'"
   else:
@@ -158,10 +114,13 @@ proc clone(src, dest: Path, srcModifcation: Time, creating: bool = false): Futur
   setLastModificationTime(string src, srcModifcation)
 
 
-proc clone(src, dest: Path, futures: var seq[Future[void]]) = # So many TOCTOU race conditions
+proc clone*(src, dest: Path, futures: var seq[Future[void]]) = # So many TOCTOU race conditions
   if dirExists(src) and not dirExists(dest): # No Future required here
     info fmt"Copying from '{src}' to '{dest}'."
-    copyDir(string src, string dest)
+    if fileExists(dest):
+      fatal fmt"Cannot copy folder to {dest} a file exists there. Delete it and restart reflector"
+    else:
+      copyDir(string src, string dest)
   elif dirExists(src) and dirExists(dest):
     for dir in walkDir(src, relative = true):
       clone(src / dir.path, dest / dir.path, futures)
@@ -178,15 +137,9 @@ proc clone(src, dest: Path, futures: var seq[Future[void]]) = # So many TOCTOU r
     futures.setLen 0 
 
 
-
-proc loadConfig(
-  watcher: cint;
-  configWatchers, watchers: var Table[cint, Path];
-  mirrors: var Table[Path, HashSet[Path]];
-  futures: var seq[Future[void]]
-) =
-  let origMirrors = mirrors.keys.toSeq.toHashSet()
-  mirrors = initTable[Path, HashSet[Path]](mirrors.len)
+proc loadConfig*(reflectObj: Reflector) =
+  let origMirrors = reflectObj.mirrors.keys.toSeq.toHashSet()
+  reflectObj.mirrors = initTable[Path, HashSet[Path]](reflectObj.mirrors.len)
   for path in cfgPaths:
     try:
       var src, dest = ""
@@ -194,19 +147,19 @@ proc loadConfig(
         if line.scanf(pathScan, src, dest):
           let (src, dest) = (Path src, Path dest)
           try:
-            clone(src, dest, futures)
+            clone(src, dest, reflectObj.futures)
           except Exception as e:
             echo e.msg
-          if mirrors.hasKeyOrPut(src, [dest].toHashset):
-            mirrors[src].incl dest
+          if reflectObj.mirrors.hasKeyOrPut(src, [dest].toHashset):
+            reflectObj.mirrors[src].incl dest
       block addIfNew:
-        for value in configWatchers.values:
+        for value in reflectObj.configWatchers.values:
           if value == path:
             break addIfNew
 
-        let fd = watcher.inotifyAddWatch(cstring path, {Modify, Create})
+        let fd = reflectObj.watcher.inotifyAddWatch(cstring path, {Modify, Create})
         if fd >= 0:
-          configWatchers[fd] = path
+          reflectObj.configWatchers[fd] = path
 
     except CatchableError as e:
       error fmt"Failed to load config from {path}: '{e.msg}'"
@@ -214,113 +167,127 @@ proc loadConfig(
 
   for mirror in origMirrors: # Handle mirrors being removed
     if mirror notin origMirrors:
-      for key, val in watchers.pairs:
+      for key, val in reflectObj.watchers.pairs:
         if val == mirror:
-          watchers.del key
+          reflectObj.watchers.del key
           break
 
+proc route*(reflectorObj: Reflector, keys: varargs[InotifyFlags],  p: proc(_: Reflector, _: ptr InotifyEvent, _: Path): Future[void] {.async.}) =
+  for key in keys:
+    reflectorObj.handlers[key] = p
 
-proc main(): Future[void] {.async.} =
-  var 
-    mirrors: Table[Path, HashSet[Path]]
-    watchesToMirror: Table[cint, Path]
-    configWatchers: Table[cint, Path]
-    futures: seq[Future[void]]
+proc dispatch*(reflectorObj: Reflector, event: ptr InotifyEvent) {.async.} =
+  if event.wd in reflectorObj.configWatchers:
+    info "Reloading config"
+    loadConfig(reflectorObj)
+    if reflectorObj.futures.len > 0:
+      await all reflectorObj.futures
+      reflectorObj.futures.setLen(0)
+  elif event.wd in reflectorObj.watchers and event.mask in reflectorObj.handlers:
+    await reflectorObj.handlers[event.mask](reflectorObj, event, reflectorObj.watchers[event.wd])
+  else:
+    unimplemented fmt" no handler for {event.mask} operating on '{event.wd}'"
 
-  let watcherFd = inotify_init1(0)
-  if watcherFd < 0:
-    fatal "Failed to intialize inotify: " & osErrorMsg(osLastError())
-    return
-  
-  loadConfig(watcherFd, configWatchers, watchesToMirror, mirrors, futures)
+proc loop*(reflectObj: Reflector) {.async.} =
+    reflectObj.watcher =inotify_init1(0)
 
-  if futures.len > 0:
-    try:
-      waitfor all futures
-    except Exception as e:
-      error e.msg
+    if reflectObj.watcher < 0:
+      fatal "Failed to intialize inotify: " & osErrorMsg(osLastError())
       return
-      
+    
+    loadConfig(reflectObj)
 
-  if mirrors.len == 0:
-    info("No files or paths to mirror")
+    if reflectObj.futures.len > 0:
+      try:
+        waitfor all reflectObj.futures
+      except Exception as e:
+        error e.msg
+        return
+        
 
-
-  var watcherFile = 
-    try:
-      newAsyncFile(AsyncFd watcherFd)
-    except CatchableError as e:
-      fatal "Could not open inotify file: " & e.msg
-      return
-
-  let startMirrors = mirrors.keys.toSeq
-  for mirror in startMirrors:
-    try:
-      watcherFd.createWatcherRecursively(mirror, watchesToMirror, mirrors, Path"")
-    except Exception as e:
-      fatal "Failed to recursively add watchers for '" & string(mirror) & "': " & e.msg
-      return
-
-  var 
-    buffer = newString(sizeof(InotifyEvent) + pathMax + 1)
-    movedFromBuffer: Table[uint32, (Path, Path)] # (Parent, Name)
-
-  info("Done syncing directories")
-  while true:
-    try:
-      let len = await watcherfile.readBuffer(buffer[0].addr, buffer.len)
-      var pos = 0
-      while pos < len:
-        var event = cast[ptr InotifyEvent](buffer[pos].addr)
-        if event.wd in configWatchers:
-          info "Reloading config"
-          loadConfig(watcherFd, configWatchers, watchesToMirror, mirrors, futures)
-
-        elif event.wd in watchesToMirror:
-          let src = watchesToMirror[event.wd]
-
-          if event.mask in [{MovedFrom}, {MovedFrom, IsDir}]:
-            if event.cookie != 0:
-              movedFromBuffer[event.cookie] = (src, Path event.getName())
-          elif event.mask in [{MovedTo, IsDir}, {MovedTo}]:
-            if event.cookie != 0:
-              let 
-                moveFrom = mirrors[movedFromBuffer[event.cookie][0]]
-                moveTo = mirrors[src]
-                srcFileName = movedFromBuffer[event.cookie][1]
-              assert moveFrom.len == moveTo.len
-              for (frm, to) in slicerator.zip(moveFrom.items, moveTo.items):
-                let
-                  src = frm / srcFileName
-                  dest = to / Path event.getName()
-                info fmt"Moving '{src}' to '{dest}'"
-                moveFile(src, dest)
-              movedFromBuffer.del event.cookie
-            else:
-              unimplemented fmt"Move to without from: {src}"
-          elif event.mask in [{Create}, {Create, IsDir}, {Modify}]:
-            let 
-              fileName = Path event.getName()
-              srcPath = watchesToMirror[event.wd]
-            for destPath in mirrors[watchesToMirror[event.wd]]:
-              let
-                src = srcPath / fileName
-                dest = destPath / fileName
-              await clone(src, dest, getLastModificationTime(string src), true)
-
-          elif event.mask in [{Attrib}]:
-            info fmt"Skipping {event.mask} unimplemented but not errory"
-          else:
-            unimplemented fmt"Failed operation {event.mask}"
+    if reflectObj.mirrors.len == 0:
+      info("No files or paths to mirror")
 
 
-        pos += sizeof(InotifyEvent) + int event.len
-      
-    except Exception as e:
-      fatal "Failed to read: " & e.msg
-      break
+    var watcherFile = 
+      try:
+        newAsyncFile(AsyncFd reflectObj.watcher)
+      except CatchableError as e:
+        fatal "Could not open inotify file: " & e.msg
+        return
 
-try:
-  waitfor main()
-except Exception as e:
-  error e.msg
+    let startMirrors = reflectObj.mirrors.keys.toSeq
+    for mirror in startMirrors:
+      try:
+        reflectObj.createWatcherRecursively(mirror, Path"")
+      except Exception as e:
+        fatal "Failed to recursively add watchers for '" & string(mirror) & "': " & e.msg
+        return
+
+    var 
+      buffer = newString(sizeof(InotifyEvent) + pathMax + 1)
+
+    info("Done syncing directories")
+    while true:
+      try:
+        let len = await watcherfile.readBuffer(buffer[0].addr, buffer.len)
+        var pos = 0
+        while pos < len:
+          var event = cast[ptr InotifyEvent](buffer[pos].addr)
+          await reflectObj.dispatch(event)
+
+          pos += sizeof(InotifyEvent) + int event.len
+        
+      except Exception as e:
+        fatal "Failed to read: " & e.msg
+        break
+
+when isMainModule:
+  addHandler newConsoleLogger()
+  addHandler newFileLogger(string (getCacheDir() / Path"reflector"))
+
+  var reflectObj = Reflector(watcher: inotify_init1(0))
+
+  reflectObj.route {MovedFrom}, {MovedFrom, IsDir}, proc(refl: Reflector, event: ptr InotifyEvent, src: Path) {.async.} = 
+    if event.cookie != 0:
+      refl.movedFromBuffer[event.cookie] = (src, Path event.getName())
+
+  reflectObj.route {MovedTo}, {MovedTo, IsDir}, proc (refl: Reflector, event: ptr InotifyEvent, src: Path) {.async.} =
+    if event.cookie != 0:
+      let 
+        moveFrom = refl.mirrors[refl.movedFromBuffer[event.cookie][0]]
+        moveTo = refl.mirrors[src]
+        srcFileName = refl.movedFromBuffer[event.cookie][1]
+      assert moveFrom.len == moveTo.len
+      for (frm, to) in slicerator.zip(moveFrom.items, moveTo.items):
+        let
+          src = frm / srcFileName
+          dest = to / Path event.getName()
+        info fmt"Moving '{src}' to '{dest}'"
+        moveFile(src, dest)
+      refl.movedFromBuffer.del event.cookie
+    else:
+      unimplemented fmt"Move to without from: {src}"
+
+  reflectObj.route {Create}, {Create, IsDir}, {Modify}, proc(refl: Reflector, event: ptr InotifyEvent, src: Path) {.async.} =
+    let 
+      fileName = Path event.getName()
+    for destPath in refl.mirrors[refl.watchers[event.wd]]:
+      let
+        src = src / fileName
+        dest = destPath / fileName
+      if IsDir in event.mask:
+        clone(src, dest, refl.futures)
+        if refl.futures.len > 0:
+          await all refl.futures
+          refl.futures.setLen(0)
+      else:
+        await clone(src, dest, getLastModificationTime(string src), true)
+
+  reflectObj.route {Attrib}, {Attrib, IsDir}, {Ignored}, {Ignored, IsDir}, proc(refl: Reflector, event: ptr InotifyEvent, _: Path){.async.} =
+    info fmt"Skipping {event.mask} unimplemented but not errory"
+
+  try:
+    waitfor reflectObj.loop()
+  except Exception as e:
+    error e.msg
